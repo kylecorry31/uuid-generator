6299c12e62a938a413c1e8f946b6af2915f07358
Kyle Corry <kylecorry31@gmail.com>
2024-01-13 19:12:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# uuid-generator
+ A simple web UUID generator
---
+++ script.js
@@ -0,0 +1,48 @@
+var NotificationManager = {
+    _notificationPane: undefined,
+    create: function (title, desc, timeout, delay) {
+        if (!timeout) {
+            timeout = 2;
+        }
+        if (!this._notificationPane) {
+            this._notificationPane = document.createElement('div');
+            this._notificationPane.classList.add('notificationPane');
+            document.body.appendChild(this._notificationPane);
+        }
+        var notification = document.createElement('div');
+        notification.classList.add('notification');
+        var context = this;
+        notification.addEventListener("click", function () {
+            context._notificationPane.removeChild(notification);
+        });
+        notification.innerHTML = '<h1 class="notification-title">' + title + '</h1><p class="notification-desc">' + desc + '</p>';
+        if (delay) {
+            setTimeout(function () {
+                this._displayNotification(notification, timeout);
+            }, delay * 1000);
+        } else {
+            this._displayNotification(notification, timeout);
+        }
+    },
+    _displayNotification: function (notification, timeout) {
+        this._notificationPane.appendChild(notification);
+        var context = this;
+        setTimeout(function () {
+            context._notificationPane.removeChild(notification);
+        }, timeout * 1000);
+    }
+};
+
+(function () {
+    document.querySelector('#uuid').textContent = crypto.randomUUID();
+
+
+    document.querySelector('#copy').addEventListener('click', function () {
+        var copyText = document.querySelector('#uuid').textContent;
+        navigator.clipboard.writeText(copyText).then(function () {
+            NotificationManager.create('Copied!', 'UUID copied to clipboard', 2);
+        }, function (err) {
+            NotificationManager.create('Error!', 'Failed to copy UUID to clipboard', 2);
+        });
+    });
+})();
